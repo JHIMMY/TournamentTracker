5c287c157f19cd85347a782529de78a4898ae1d9
Jhimmy <jadsa_corp@hotmail.com>
2022-07-26 14:33:14 -0400

    Created a class library with various models for the project

---
+++ TrackerLibrary/Models/PersonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrackerLibrary.Models;
+public class PersonModel
+{
+    /// <summary>
+    /// Represents the first name of this particular person
+    /// </summary>
+    public string FirstName { get; set; }
+
+    /// <summary>
+    /// Represents the last name of this particular person
+    /// </summary>
+    public string LastName { get; set; }
+
+    /// <summary>
+    /// Represents the email address of this particular person
+    /// </summary>
+    public string EmailAddress { get; set; }
+
+    /// <summary>
+    /// Represents the cell phone number of this particular person
+    /// </summary>
+    public string CellphoneNumber { get; set; }
+}
